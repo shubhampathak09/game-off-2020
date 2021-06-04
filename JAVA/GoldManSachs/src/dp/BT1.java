8d25fe88fb0179a30a3da30358eb78e17909c22f
shubham pathak <95shubham1@gmail.com>
2021-06-04 14:30:18 +0530

    codes

---
+++ JAVA/GoldManSachs/src/dp/BT1.java
@@ -0,0 +1,161 @@
+package dp;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BT1 {
+	
+	
+	public static class Node
+	{
+		int data;
+		Node left;
+		Node right;
+	}
+
+	public static Node newNode(int data)
+	{
+		Node node=new Node();
+		node.data=data;
+		node.left=null;
+		node.right=null;
+		
+		return node;
+	}
+	
+	
+	public static void inorder(Node root)
+	{
+		
+		if(root==null)
+			return;
+		
+		inorder(root.left);
+		
+		System.out.print(root.data+" ");
+		
+		inorder(root.right);
+		
+	}
+	
+	public static void preorder(Node root)
+	{
+		
+		if(root==null)
+			return;
+			
+		System.out.print(root.data+" ");
+		preorder(root.left);
+		preorder(root.right);
+			
+	}
+	
+	public static int maxDepth(Node root)
+	{
+		
+		if(root==null)
+			return 0;
+		
+		int lh=maxDepth(root.left);
+		int rh=maxDepth(root.right);
+		
+		return 1+Math.max(lh,rh);
+	}
+	
+	public static void postorder(Node root)
+	{
+		if(root==null)
+			return;
+		postorder(root.left);
+		postorder(root.right);
+		System.out.print(root.data+" ");
+	}
+	
+	public static int di=Integer.MIN_VALUE;
+	
+	public static int diameter(Node root)
+	{
+		
+		if(root==null)
+			return 0;
+		
+	    int ld=diameter(root.left);
+	    int rd=diameter(root.right);
+		int temp=Math.max(ld,rd)+1;
+		int ans=Math.max(ld+rd+1, temp);
+		di=Math.max(di, ans);
+		return ans;
+	}
+	
+	public static void ZigZagTraversal(Node root)
+	{
+		
+	}
+	
+	public static void levelorder(Node root)  // review ??
+	{
+		
+		Queue<Node>q=new LinkedList<>();
+		
+		q.add(root);
+		
+		while(q.isEmpty()==false)
+		{
+			
+		int sz=q.size();
+		
+//		if(sz==0)
+//			break;
+		
+		while(sz>0)
+		{
+		 
+		Node front=q.peek();
+		q.remove();
+		System.out.print(front.data+" ");
+		if(front.left!=null)
+			q.add(front.left);
+		if(front.right!=null)
+			q.add(front.right);
+		
+	//	System.out.println();
+		}
+		System.out.println();
+		}
+		
+		
+	}
+	
+	// max sum from leaf to lead
+	
+	// max sum from node to node
+	
+	
+	
+	public static void main(String[] args)
+	{
+		
+		Node root=newNode(1);
+		root.left=newNode(2);
+		root.right=newNode(3);
+		root.left.left=newNode(4);
+		root.left.right=newNode(5);
+		root.right.right=newNode(6);
+		
+//		
+//		inorder(root);
+//		System.out.println();
+//		postorder(root);
+//		System.out.println();
+//		preorder(root);
+		
+		System.out.println();
+		
+		System.out.println(maxDepth(root));
+		
+		
+		diameter(root);
+		//levelorder(root);
+		System.out.println(di);
+	}
+}
