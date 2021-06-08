5db92ac88f2f383cd8b7ec701945089be59ac4cf
shubham pathak <95shubham1@gmail.com>
2021-06-08 09:59:27 +0530

    Linked List codes

--- JAVA/GoldManSachs/src/dp/ImpLinkedList.java
+++ JAVA/GoldManSachs/src/dp/ImpLinkedList.java
@@ -69,6 +69,35 @@ public class ImpLinkedList {
 			}
 		}
 		
+		public static void removeLast()
+		{
+		
+			
+			if(size==0)
+			{
+				System.out.println("List is empty cant remove");
+				return;
+			}
+			
+			if(size==1)
+			{
+				head=null;
+				size=size-1;
+				
+			}
+			else
+			{
+				Node secondlast=head;
+				
+				for(int i=1;i<=size-2;i++)
+				{
+					secondlast=secondlast.next;
+				}
+		        secondlast.next=null;
+		        size=size-1;
+			}
+		}
+		
 		public static void removeFirst(Node node)
 		{
 			// 1- 2 3 4 5 6
@@ -98,6 +127,124 @@ public class ImpLinkedList {
 				
 				return;
 			}
+		}
+		
+		public static  int getFirst()
+		{
+			
+			
+			if(size==0)
+			{
+				System.out.println("No elements in Linked List");
+				return -1;
+			}
+			
+			return head.data;
+			
+		}
+		
+		public static void addFirst(int data)
+		{
+			
+			Node temp=new Node(data);
+			
+			temp.next=head;
+			head=temp;
+			size=size+1;
+		}
+		
+		public static  int getLast()
+		{
+			
+			
+			if(size==0)
+			{
+				System.out.println("No elements in Linked List");
+				return -1;
+			}
+			
+			Node temp=head;
+			
+		    while(temp.next!=null)
+		    {
+		    	temp=temp.next;
+		    }
+		    
+			return temp.data;
+		}
+		
+		public static void addAtIndex(int index,int data)
+		{
+			
+			if(index<0&&index>=size)
+			{
+				System.out.println("cant add at invalid index");
+				return;
+			}
+			
+			
+			
+			
+			if(index==0)
+			{
+				// tryijng addition at first index
+			  addFirst(data);
+				
+			}
+			else
+			{
+		    Node temp=new Node(data);
+            Node currnode=head;
+            for(int i=1;i<=index-1;i++) // got index-1
+            {
+            	currnode=currnode.next;
+            }
+            Node currnextnode=currnode.next;
+            currnode.next=temp;
+            temp.next=currnextnode;
+            
+            size=size+1;
+			}
+		}
+		
+		public static int getAtIndex(int index)
+		{
+			if(size==0)
+			{
+				System.out.println("Linked List is Empty");
+				return -1;
+			}
+			if(index<0||index>=size)
+			{
+				System.out.println("Invalid Index");
+				return -1;
+			}
+			
+			Node temp=head;
+			for(int i=1;i<=index;i++)
+			{
+				
+				temp=temp.next;
+			}
+			return temp.data;
+		}
+		
+		
+		public static void reverse()
+		{
+			Node current=head;
+		    Node prev=null;
+		   // Node temp=current.next;
+			while(current!=null)
+			{
+				
+		    Node temp=current.next;
+			current.next=prev;
+			prev=current;
+			current=temp;
+			}
+			head=prev;
+		    
 		}
 		
 		public static void main(String[] args)
@@ -150,6 +297,60 @@ public class ImpLinkedList {
 		    removeFirst(head);
 		    
 		    System.out.println(" Head is "+head.data+" size "+size);
+		    
+		    
+		    // 3
+		    int val=getAtIndex(1);
+		    
+		    System.out.println(val);
+		    
+		    System.out.println(getFirst());
+		    
+		    System.out.println(getLast());
+		    
+		    
+		    
+		    //4
+		    
+		    
+		    // add at first
+		    
+		    addFirst(9);
+		    
+		  
+		    
+		    display();
+		    
+		    System.out.println();
+		    System.out.println(size);
+		    
+		    
+		    //5 AddatIndex function
+		    
+		    addAtIndex(2,8);
+		    
+		    // 6 remove at index;
+		    
+		    System.out.println();
+		    System.out.println(size);
+		    display();
+		    
+		    System.out.println();
+		    
+		    removeLast();
+		    
+		    display();
+		    
+		    System.out.println();
+		    
+		   
+		   // 7
+		   reverse();
+		   display();
+		    System.out.println(head.data);
+		    
+		   
+		    
 		}
 		
 	}
@@ -157,5 +358,10 @@ public class ImpLinkedList {
 	
 	// 3
 	
+    //  int val=getAtIndex(2);	
+      //8
 	
-}
+	 
+	}
+	
+
---
+++ JAVA/GoldManSachs/src/dp/LinkedListStackAdaptor.java
@@ -0,0 +1,8 @@
+package dp;
+
+public class LinkedListStackAdaptor {
+	
+	
+	
+
+}
