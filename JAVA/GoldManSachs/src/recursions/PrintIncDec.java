fe9b3b2f7d7bfbf0adac300016a4eb752b67e166
shubham pathak <95shubham1@gmail.com>
2021-07-03 21:02:01 +0530

    printdec

--- JAVA/GoldManSachs/src/recursions/PrintIncDec.java
+++ JAVA/GoldManSachs/src/recursions/PrintIncDec.java
@@ -1,5 +1,35 @@
 package recursions;
 
+import java.util.Scanner;
+
 public class PrintIncDec {
+	
+	
+	public static void printDec(int n)
+	{
+		
+		if(n==1)
+		{
+			System.out.println(1);
+			return;
+		}
+		
+		System.out.println(n);
+		printDec(n-1);
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		
+		Scanner S=new Scanner(System.in);
+		
+		int n=S.nextInt();
+		
+		
+		
+		printDec(n);
+		
+	}
 
 }
