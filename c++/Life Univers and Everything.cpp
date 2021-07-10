7625ede067471a3a2d924debe77cbbe60eb71305
shubham pathak <95shubham1@gmail.com>
2021-07-10 22:22:56 +0530

    Life,Universe,Everything

---
+++ c++/Life Univers and Everything.cpp
@@ -0,0 +1,21 @@
+// Life Universe and Everything
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	int n;
+	
+	while(cin>>n)
+	{
+		if(n==42)
+		break;
+		else
+		{
+			cout<<n<<endl;
+		}
+		
+	}
+}
