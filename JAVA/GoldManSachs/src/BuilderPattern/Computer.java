cd926bb7a24727bb819bea38aec618d9c84f52bd
shubham pathak <95shubham1@gmail.com>
2021-07-22 16:02:36 +0530

    builder pat

--- JAVA/GoldManSachs/src/BuilderPattern/Computer.java
+++ JAVA/GoldManSachs/src/BuilderPattern/Computer.java
@@ -1,5 +1,44 @@
-package BuilderPattern;
-
-public class Computer {
-
-}
+//package BuilderPattern;
+//
+//public class Computer {
+//
+//	// required parameters
+//	
+//	private String HDD;
+//	private String RAM;
+//	
+//	// optional paramters
+//	
+//	private boolean isGraphicsCardEnabled;
+//	private boolean isBluetoothEnabled;
+//	
+//	public String getHDD()
+//	{
+//		return HDD;
+//	}
+//	
+//	public String getRAM()
+//	{
+//		return RAM;
+//	}
+//	
+//	public boolean isGraphicsCardEnabled()
+//	{
+//		return isGraphicsCardEnabled;
+//	}
+//	
+//	public boolean isBluetoothEnabled()
+//	{
+//		return isBluetoothEnabled;
+//	}
+//	
+//	private Computer(ComputerBuilder builder)
+//	{
+//		this.HDD=builder.HDD;
+//		this.RAM=builder.RAM;
+//		
+//		
+//		this.isGraphicsCardEnabled=builder.isGraphicsCardEnabled;
+//		this.isBluetoothEnabled=builder.isBluetoothEnabled;
+//	}
+//}
--- JAVA/GoldManSachs/src/BuilderPattern/ComputerBuilder.java
+++ JAVA/GoldManSachs/src/BuilderPattern/ComputerBuilder.java
@@ -1,5 +1,33 @@
-package BuilderPattern;
-
-public class ComputerBuilder {
-
-}
+//package BuilderPattern;
+//
+//public static class ComputerBuilder {
+//
+//	
+//	private String HDD;
+//	private String RAM;
+//	
+//	private boolean isGraphicsCardEnabled;
+//	
+//	private boolean isBluetoothEnabled;
+//	
+//	
+//	public ComputerBuilder(String hdd,String ram)
+//	{
+//	this.HDD=hdd;
+//	this.RAM=ram;
+//	}
+//	
+//	
+//	public ComputerBuilder setBluetoothEnabled(boolean isBletoothEnabled)
+//	{
+//		this.isBluetoothEnabled=isBletoothEnabled;
+//		
+//		return this;
+//	}
+//	
+//	public Computer build()
+//	{
+//		return new Computer(this);
+//	}
+//	
+//}
